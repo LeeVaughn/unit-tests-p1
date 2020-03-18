5aae1dd10c9b9b47d7665b66c0a048d7c8579215
LeeVaughn <dlvprogramming@gmail.com>
2020-03-18 14:31:37 -0700

    created initial test suites with comments as pseudo code

--- test/test.js
+++ test/test.js
@@ -7,3 +7,36 @@ describe("Mocha", function() {
     expect(true).to.be.ok;
   });
 });
+
+describe("Array of Objects", function() {
+  // is named quotes
+  // contains at least five quotes
+  // is free of errors
+});
+
+describe("Object Properties", function() {
+  // all objects have quote and source properties
+  // at least one object has citation property
+  // at least one object has a year property
+  // (EXCEED) at least one object has an additional property
+});
+
+describe("getRandomQuote Function", function() {
+  // is named getRandomQuote
+  // returns a random object from the quotes array
+});
+
+describe("printQuote Function", function() {
+  // is named printQuote
+  // calls getRandomQuote function
+  // prints quote and source properties with every quote
+  // prints citation property with at least one quote
+  // prints year property with at least one quote
+  // printed quotes match template layout
+  // (EXCEEDS) quotes automatically refresh at regular intervals
+  // (EXCEEDS) background color changes each time a new quote is displayed
+});
+
+describe("Code Comments", function() {
+  // code includes comments
+});
