ccdf9151c5ee123a8741ba9eeef4d7252750a1e2
LeeVaughn <dlvprogramming@gmail.com>
2020-03-17 17:13:48 -0700

    set up initial test suite and ensured it was working

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const expect = require("chai").expect;
+
+// test suite
+describe("Mocha", function() {
+  // test spec
+  it("should run our tests using npm", function() {
+    expect(true).to.be.ok;
+  });
+});
